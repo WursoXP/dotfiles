b8d3eff0e71ec7d96fb766fa51ef78da95772655
WursoXP <paulosergio2005@outlook.com>
2026-03-23 16:41:56 -0300

    concertei o treesitter (copiei do primeagen kkj)

--- .config/nvim/lua/xp/lazy/lsp.lua
+++ .config/nvim/lua/xp/lazy/lsp.lua
@@ -24,7 +24,6 @@ return { {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "gopls",
                 "clangd",
                 "ty",
                 "glsl_analyzer",
