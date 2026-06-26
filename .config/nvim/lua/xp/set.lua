63c6d1a4eb60bf1567fefdfa83711057e16e2df5
WursoXP <paulosergio2005@outlook.com>
2026-06-26 17:38:06 -0300

    kitty font size, nvim config?

--- .config/nvim/lua/xp/set.lua
+++ .config/nvim/lua/xp/set.lua
@@ -34,10 +34,3 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"copilot-chat"},
-  callback = function()
-      require("treesitter-context").disable()
-  end,
-})
